ace186646380868f7c48881417f4c7ac7cd8d70d
gabi <gaby.gabyyy29@gmail.com>
2022-08-20 00:29:57 +0300

    Build initial logic

---
+++ ui.js
@@ -0,0 +1,22 @@
+function clearCurrentGrid()
+{
+  gridEl.innerHTML = '';
+}
+
+function updateRangeLabel(size=16)
+{
+  rangeLabel.textContent = `${size} x ${size}`;
+}
+
+function renderNewGrid(size=16)
+{
+  gridEl.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridEl.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for(let i = 1; i<=size * size; i++)
+  {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    gridEl.appendChild(square);
+  }
+}
